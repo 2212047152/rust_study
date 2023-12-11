c69358cc774f3dd2a05e2f0602d8e1b6ce7ad85e
lmyykxy <lmyykxy@163.com>
2023-12-11 15:44:25 +0800

    finish greeting

--- greeting/src/main.rs
+++ greeting/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    // printf with new line
+	println!("Hello, world!");
 
+	// print variable
 	let a = 12;
 	println!("a is {}",a);
 	println!("a is {0} repeat print {0}",a);
