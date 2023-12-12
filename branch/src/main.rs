26cb833eec3725f1cbe734eadf0ae6974bb48346
lmyykxy <lmyykxy@163.com>
2023-12-12 14:35:33 +0800

    modify main.rs for if and while

--- branch/src/main.rs
+++ branch/src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
-    println!("Hello, world!");
+	// if condition
+	let number = 3;
+	if number < 5 {
+		println!("IF true");
+	} else {
+		println!("IF false");
+	}
+
+	// if bool condition
+	let condition = true;
+	if condition {
+		println!("Condition true");
+	} else {
+		println!("Condition false");
+	}
+
+	// if tristate expression
+	let result = if condition {true} else {false};
+	println!("Result is {}",result);
+
+	// while condition
+	let mut flag = 1;
+	while flag != 4 {
+		flag += 1;
+		println!("flag is {flag}")
+	}
+
+	// for condition
+	
 }
